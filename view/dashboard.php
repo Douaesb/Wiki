8a5672c4128e52cfd2b416dc141ea23b16cc95b5
Douae sb <douae.sb411@example.com>
2024-01-08 19:29:37 +0100

    tag front

--- view/dashboard.php
+++ view/dashboard.php
@@ -41,13 +41,13 @@ $user->logout();
 
 
                 <li>
-                    <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                    <a href="categories.php" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                         <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-shopping-bag"></i></span>
                         <span class="text-sm font-medium">Categories</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                    <a href="tags.php" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                         <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-tag"></i></span>
                         <span class="text-sm font-medium">tags</span>
                     </a>
