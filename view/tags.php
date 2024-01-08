6c33edc81921efeb324d6b0b2936edd22326d606
Douae sb <douae.sb411@gmail.com>
2024-01-08 20:55:16 +0100

    wiki front

--- view/tags.php
+++ view/tags.php
@@ -52,6 +52,12 @@ $user->logout();
                         <span class="text-sm font-medium">tags</span>
                     </a>
                 </li>
+                <li>
+                    <a href="../index.php" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                        <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-book-open"></i></span>
+                        <span class="text-sm font-medium">Wikis</span>
+                    </a>
+                </li>
                 <!-- <li>
                     <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                         <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-user"></i></span>
